c0ebfd2f4c3dd31a45147a30a80a05d9d240e5fc
kangactor123 <kangactor123@naver.com>
2024-08-15 15:03:47 +0900

    module-federation-redux-example

---
+++ mfa-module-federation-redux/apps/main-app/src/App.tsx
@@ -0,0 +1,36 @@
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom/client";
+
+import "./index.css";
+import { Provider, useDispatch, useSelector } from "react-redux";
+import createStore from "./redux/store";
+import { decrement, increment } from "./redux/modules/counter";
+
+const RemoteApp = React.lazy(() => import("remote_app/RemoteApp"));
+
+const { store, injectReducer } = createStore();
+
+const App = () => {
+  const counter = useSelector<ReturnType<typeof store.getState>, number>(
+    (state) => state.counter.value
+  );
+  const dispatch = useDispatch();
+  return (
+    <div className="container">
+      <div>{counter}</div>
+      <div>
+        <button onClick={() => dispatch(increment())}>+</button>
+        <button onClick={() => dispatch(decrement())}>-</button>
+      </div>
+      <Suspense fallback={<div>loading...</div>}>
+        <RemoteApp store={store} injectReducer={injectReducer} />
+      </Suspense>
+    </div>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("app")!).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
